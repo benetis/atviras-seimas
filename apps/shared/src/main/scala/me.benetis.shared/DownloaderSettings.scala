f89d625ccbfd554f07c8eba9cf1fbfbd20475774
Zygimantas <zygimantas@benetis.me>
2019-01-10 19:18:08 +0200

    feat(shared) planned data structures for sharing

--- apps/shared/src/main/scala/me.benetis.shared/DownloaderSettings.scala
+++ apps/shared/src/main/scala/me.benetis.shared/DownloaderSettings.scala
@@ -4,11 +4,42 @@ import org.joda.time.DateTime
 
 sealed trait DownloaderSettings
 
+case class IndividualId(value: String)
+case class IndividualName(value: String)
+case class IndividualSurname(value: String)
+case class IndividualFraction(value: String)
 
-/**
-  * <EigosKlausimas laikas_nuo="15:07" numeris="2 - 7." svarstomo_klausimo_id="-31496"
-  * stadija="Priėmimas" pavadinimas="Su nekilnojamuoju turtu susijusio kredito įstatymo Nr. XII-2769 12 ir 16 straipsnių pakeitimo įstatymo projektas (Nr. XIIIP-1712(2))"/>
-  */
+sealed trait SingleVote
+case object VoteFor extends SingleVote
+case object VoteAgainst extends SingleVote
+case object VoteAbstain extends SingleVote
+case object DidntVote extends SingleVote
+
+case class SingleVoteResult(
+    individualId: IndividualId,
+    name: IndividualName,
+    surname: IndividualSurname,
+    fraction: IndividualFraction,
+    vote: SingleVote
+)
+
+case class VoteTime(value: DateTime)
+case class VoteTotal(value: Int)
+case class VoteTotalMax(value: Int)
+case class VoteFor(value: Int)
+case class VoteAgainst(value: Int)
+case class VoteAbstained(value: Int)
+case class VoteComment(value: String)
+case class VoteId(value: String)
+
+case class VoteResults(voteId: VoteId,
+                       voteTime: VoteTime,
+                       voteTotal: VoteTotal,
+                       voteTotalMax: VoteTotalMax,
+                       voteFor: VoteFor,
+                       voteAgainst: VoteAgainst,
+                       voteAbstained: VoteAbstained,
+                       voteComment: VoteComment)
 
 case class PlenaryQuestionId(value: String)
 case class PlenaryQuestionTitle(value: String)
@@ -24,8 +55,7 @@ case class PlenaryQuestion(id: PlenaryQuestionId,
                            title: PlenaryQuestionTitle,
                            timeFrom: PlenaryQuestionTimeFrom,
                            number: PlenaryQuestionNumber,
-                           status: PlenaryQuestionStatus
-                          )
+                           status: PlenaryQuestionStatus)
 
 case class PlenaryId(value: String)
 case class Plenary(id: PlenaryId, sessionId: SessionId)
