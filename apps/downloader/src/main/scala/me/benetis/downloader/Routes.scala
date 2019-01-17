24f283801fa77fa7da689e345ec9dc823763146c
Zygimantas <zygimantas@benetis.me>
2019-01-17 16:34:13 +0200

    feat(chore) cleanup

--- apps/downloader/src/main/scala/me/benetis/downloader/Routes.scala
+++ apps/downloader/src/main/scala/me/benetis/downloader/Routes.scala
@@ -6,10 +6,10 @@ import org.http4s.HttpRoutes
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 
-class HelloWorldRoutes[F[_]: Sync] extends Http4sDsl[F] {
+class Routes[F[_]: Sync] extends Http4sDsl[F] {
   val routes: HttpRoutes[F] =
     HttpRoutes.of[F] {
       case GET -> Root / "hello" / name =>
-        Ok(Json.obj("message" -> Json.fromString(s"Hello, ${name}")))
+        Ok(Json.obj("message" -> Json.fromString(s"Hello , ${name}")))
     }
 }
--- apps/downloader/src/main/scala/me/benetis/downloader/Server.scala
+++ apps/downloader/src/main/scala/me/benetis/downloader/Server.scala
@@ -7,14 +7,14 @@ import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.syntax.kleisli._
 
-object HelloWorldServer extends IOApp {
+object Server extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
     ServerStream.stream[IO].compile.drain.as(ExitCode.Success)
 }
 
 object ServerStream {
   def helloWorldRoutes[F[_]: Effect]: HttpRoutes[F] =
-    new HelloWorldRoutes[F].routes
+    new Routes[F].routes
 
   def stream[F[_]: ConcurrentEffect: Timer]: fs2.Stream[F, ExitCode] =
     BlazeServerBuilder[F]
