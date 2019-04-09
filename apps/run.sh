a32aab4974582bae65fce531861068d0af67b715
zygis <benetis.zygimantas@gmail.com>
2019-04-10 00:34:44 +0300

    feat(k-means) run commands

--- apps/run.sh
+++ apps/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-java -Dconfig.file=application.properties -jar coordinator-assembly-0.0.1-SNAPHOT.jar
+java -Dconfig.file=application.properties -jar coordinator-assembly-0.0.1-SNAPSHOT.jar
 
 docker run --net=host
